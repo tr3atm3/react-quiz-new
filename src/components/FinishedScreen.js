9a902d4caa9ad1128d38c0e7b2f1d75e51a74ffe
Tushar Sharma <tttushar.789@gmail.com>
2024-07-10 18:15:05 +0530

    complete

---
+++ src/components/FinishedScreen.js
@@ -0,0 +1,20 @@
+function FinishedScreen({ points, maxPossiblePoints, highscore }) {
+  const percentage = (points / maxPossiblePoints) * 100;
+  let emoji;
+  if (percentage === 100) emoji = "🥇";
+  if (percentage >= 80 && percentage < 100) emoji = "🥈";
+  if (percentage >= 50 && percentage < 80) emoji = "🥉";
+  if (percentage > 0 && percentage < 50) emoji = "👌";
+  if (percentage === 0) emoji = "❌";
+  return (
+    <>
+      <p className="result">
+        <span>{emoji}</span>You Scored <strong>{points}</strong> out of{" "}
+        {maxPossiblePoints} ({Math.ceil(percentage)}%)
+      </p>
+      <p className="highscore">Highscore : {highscore} points</p>
+    </>
+  );
+}
+
+export default FinishedScreen;
